e584acd222e090e99d9d94f8e54657e3cbacda22
Mohmett <mohmettjama@gmail.com>
2026-01-04 11:30:14 +0300

    Exercise Two Completed

---
+++ Exercise 2/app.js
@@ -0,0 +1,13 @@
+// Exercise One
+
+let x = 7;
+let y = 3;
+
+console.log("Addition",x+y);
+console.log("Substraction",x-y);
+console.log("Multiplication",x*y);
+console.log("Division",x/y);
+console.log("Reminder",x%y);
+console.log("X Increment",x++);
+console.log("Y Decrement",y--);
+
