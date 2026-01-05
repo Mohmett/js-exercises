590f9718e2a678db045d339ed2af4fbf5b040a82
Mohmett <mohmettjama@gmail.com>
2026-01-05 11:54:25 +0300

    add function comments

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,11 +1,12 @@
 // Exercise 4
 
+// Void Function
 function add(num1,num2){
     console.log(num1+num2);
 }
 add(20,30);
 
-
+// Expression Function
 const addition=function(pens,books){
     console.log("the total of Books and Pens are:", pens+books);
 }
