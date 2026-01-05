f2da1dc09d77823a039c46c9aecf0630b2175d74
Mohmett <mohmettjama@gmail.com>
2026-01-05 14:30:17 +0300

    Exercise 7 assigned

---
+++ Exercise7/app.js
@@ -0,0 +1,18 @@
+// Exercise 6
+
+// Arrays
+
+let colors=["Red","Green","Blue"];
+
+console.log(colors);
+
+// Remove the first element
+colors.shift();
+console.log(colors);
+
+// Add an element to end of the array
+colors.push("White")
+console.log(colors);
+
+
+
