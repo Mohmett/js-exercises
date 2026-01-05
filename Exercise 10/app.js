28e5447641ab982214ae2b9f017f39a44da7a1ce
Mohmett <mohmettjama@gmail.com>
2026-01-05 15:25:08 +0300

    Exercise 10 assigned

---
+++ Exercise 10/app.js
@@ -0,0 +1,32 @@
+// Exercise 10
+
+// Arrays of Objects
+
+
+let library=[
+    {
+        title:"Hello World!",
+        author:"Mohamed Jama",
+        year: 2025,
+    },
+    {
+        title:"Digital World",
+        author:"Marcozz",
+        year: 1990,
+    }
+]
+
+console.log(library);
+console.log(library[0]);
+console.log(library[0].title);
+console.log(library[0].author);
+console.log(library[0].year);
+
+console.log(library[1]);
+console.log(library[1].title);
+console.log(library[1].author);
+console.log(library[1].year);
+
+
+
+
