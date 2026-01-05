9a422876e0ac7d0b48c778227b911fb399f4c4ad
Mohmett <mohmettjama@gmail.com>
2026-01-05 11:51:54 +0300

    Exercise 4 assigned

---
+++ Exercise 4/app.js
@@ -0,0 +1,12 @@
+// Exercise 4
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+add(20,30);
+
+
+const addition=function(pens,books){
+    console.log("the total of Books and Pens are:", pens+books);
+}
+addition(23,23);
