fe596a03dec8467fd725d0b6024fc9420eb645c5
Mohmett <mohmettjama@gmail.com>
2026-01-05 14:53:22 +0300

    Exercise 8 assigned

---
+++ Exercise 8/app.js
@@ -0,0 +1,14 @@
+// Exercise 8
+
+// Objects
+
+let car={
+    Name:"Toyota",
+    model: 1967,
+    year: "H200",
+};
+
+console.log(car);
+console.log(car.Name);
+console.log(car.model);
+console.log(car.year);
