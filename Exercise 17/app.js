e6533661048f24ae041f98ea0ccf8c7c1fee1150
Mohmett <mohmettjama@gmail.com>
2026-01-09 18:38:47 +0300

    Exercise 17 assigned

---
+++ Exercise 17/app.js
@@ -0,0 +1,13 @@
+// Exercise 17
+
+let temprature=18;
+if(temprature<0){
+    console.log("it is very cold");
+} else if(temprature >=0 && temprature<=15 ){
+    console.log("it is Cold");
+} else if(temprature>=15 && temprature<=25){
+    console.log("It is Warm")
+} 
+else {
+    console.log("it is hot");
+}
