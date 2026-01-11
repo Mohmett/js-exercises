685477b8168dbb38d2c2f1ba3c300bc5671072b6
Mohmett <mohmettjama@gmail.com>
2026-01-11 10:54:30 +0300

    Exercise 23 assigned

---
+++ Exercise 23/app.js
@@ -0,0 +1,7 @@
+// Exercise 23
+
+
+let car={make:"Toyota", model:"Corolla", year:2025}
+
+const {make,model,year}=car;
+console.log(`This car made: ${make} and its model is: ${model} and made in: ${year}`);
