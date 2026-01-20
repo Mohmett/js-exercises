5c5dd5f7659eec1d14ffb3f408eddfa566ed728f
Mohmett <mohmettjama@gmail.com>
2026-01-20 21:12:34 +0300

    Exercise 32 assigned

---
+++ Exercise 32/app.js
@@ -0,0 +1,14 @@
+// Exercise 32
+
+
+// ById
+const image=document.querySelector("#avatar");
+console.log(image)
+
+// ByClass
+const infoo=document.querySelectorAll(".infoo");
+console.log(infoo);
+
+// ByTagName
+const paragraphs=document.querySelectorAll('p');
+console.log(paragraphs)
